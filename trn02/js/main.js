67173e4f9ce5d509ca94999826b2013aaa9739f7
nk12nk <104603762+nk12nk@users.noreply.github.com>
2022-05-11 14:10:06 +0900

    Add files via upload

--- trn02/js/main.js
+++ trn02/js/main.js
@@ -18,31 +18,80 @@ $(function () {
         pauseOnFocus: false,
     });
 
-    $('.movie .dec i:nth-of-type(1)').on('click', function(){
+    $('.movie .dec i:nth-of-type(1)').on('click', function () {
         $('.movie video').trigger('play');
     });
 
-    $('.movie .dec i:nth-of-type(2)').on('click', function(){
+    $('.movie .dec i:nth-of-type(2)').on('click', function () {
         $('.movie video').trigger('pause');
     });
 
     $('#myMovie').YTPlayer({
-        videoURL:'https://youtu.be/qrshRevYiiA',
-        containment:'.utube',
-        autoPlay:true, 
-        mute:false, 
-        startAt:0, 
-        opacity:1,
+        videoURL: 'https://youtu.be/qrshRevYiiA',
+        containment: '.utube',
+        autoPlay: true,
+        mute: false,
+        startAt: 0,
+        opacity: 1,
         showControls: false,
         playOnlyIfVisible: true
     });
 
-    $('.utube i:nth-of-type(1)').on('click', function(){
+    $('.utube i:nth-of-type(1)').on('click', function () {
         $('#myMovie').YTPPlay();
     });
 
-    $('.utube i:nth-of-type(2)').on('click', function(){
+    $('.utube i:nth-of-type(2)').on('click', function () {
         $('#myMovie').YTPPause();
     });
+
+    /*
+        $('.product_slider').on('init reInit afterChange', function (e, s, c) {
+            let current = $('.product-slider .slick-current');
+            current.addClass('on').siblings().removeClass('on')
+        })
+    */
+
+    $('.product_slider').slick({
+        slidesToShow: 5,
+        centerMode: true,
+        arrows: false,
+        dots: true,
+        autoplay: true,
+    });
+
+    $('.product_list .s_left').on('click', function () {
+        $('.product_slider').slick('slickPrev');
+    });
+
+    $('.product_list .s_right').on('click', function () {
+        $('.product_slider').slick('slickNext');
+    });
+
+    $('.tab_link>li').on('click', function () {
+        var idx = $(this).index();
+        $(this).addClass('on').siblings().removeClass('on');
+        $('.tab_content>li').eq(idx).addClass('on').siblings().removeClass('on');
+    });
+
+    /*새창에서 띄우기 window.open , 같은창에서 띄우기 document.open*/
+    $('.footer #link').on('change', function () {
+        var lik = $(this).val();
+        if(lik) window.open(lik)
+    })
+
     ///////////////////////////////////////////////////
-})
+});
+
+
+
+
+
+
+
+
+
+
+
+
+

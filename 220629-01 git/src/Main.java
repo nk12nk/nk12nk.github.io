dc999d51790aba8ffb110a35559e4b01b09c1fb7
Administrator <Administrator@SC-202106161522>
2022-06-29 10:20:26 +0900

    커밋 함 해봄~

--- 220629-01 git/src/Main.java
+++ 220629-01 git/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		// 헬로우월드
 		// 커밋 변경점찍기asdfasfd
-		// 두번째 줄 적음asdfdasf
+		// 두번째 줄 적음asdfdasfㅁ
 		System.out.println("Hello World!");
 		System.out.println("Hello World!");
 		System.out.println("Hello World!");

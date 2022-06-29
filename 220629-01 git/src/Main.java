c20dc5817cd6953789583b441989664157ee73b8
Administrator <Administrator@SC-202106161522>
2022-06-29 10:22:06 +0900

    힝입니다

--- 220629-01 git/src/Main.java
+++ 220629-01 git/src/Main.java
@@ -3,8 +3,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		// 헬로우월드
-		// 커밋 변경점찍기asdfasfd
-		// 두번째 줄 적음asdfdasfㅁ
+		// 커밋 변경점찍기
+		// 두번째 줄 적음
 		System.out.println("Hello World!");
 		System.out.println("Hello World!");
 		System.out.println("Hello World!");
